a1b8a417d799a929ec1cb420d9c915a6f0ae13f1
otownsend92 <otownsend92@gmail.com>
2015-11-21 16:42:47 -0800

    fixed script

--- user_data.py
+++ user_data.py
@@ -6,11 +6,17 @@ except:
 from instagram.client import InstagramAPI
 import os
 import urllib
+import time
 
 
 # Insert access_token and client secret here:
 access_token = ""
 client_secret = ""
+
+# TODO: INSERT FILE NAME HERE!!!
+file_name = 'all_media_single_file.txt'
+user_data = {}
+file_count = 0;
 
 def user(user_id):
 	num_followed_by = 0
@@ -26,28 +32,31 @@ def user(user_id):
 		print(e)
 	return num_followed_by,num_follows,num_media
 
+def write_file():
+	write_file = "user_data_" + str(file_count) + ".txt"
+	fw = open(write_file, 'w')
+	pickle.dump(user_data, fw)
+	file_count = file_count+1
+	fw.close()
 
-user_data = {}
 
-# TODO: INSERT FILE NAME HERE!!!
-file_name = ''
 
 f = open(file_name, 'r')
 print("Reading file: " + file_name)
 print(f)
 curr_media = pickle.load(f)
+f.close()
 for key in curr_media:
 	user_id = curr_media[key].user.id
 	num_followed_by,num_follows,num_media = user(user_id)
 	user_data[user_id] = (num_followed_by,num_follows,num_media)
-f.close()
+	time.sleep(0.73)
+	if(len(user_data) == 30000):
+		write_file()
+		user_data.clear()
 
 
-print len(user_data), ' unique users. Writing data to disk...'
 
-f = open("user_data.txt", 'w')
-pickle.dump(user_data, f)
-f.close()
 
 print 'Done.\n'
 
