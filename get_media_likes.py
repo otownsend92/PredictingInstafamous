6a3c8cd0130dda7978664ddcae95fb3ef6e96fa6
Zenghui Yan <yanzenghui920@gmail.com>
2015-12-01 20:39:00 -0800

    get media_id likes dictionary

---
+++ get_media_likes.py
@@ -0,0 +1,45 @@
+try:
+	import cPickle as pickle
+except:
+	import pickle
+
+import os
+
+
+file_name = 'all_media_single_file.txt'
+media_data = {}
+media_like = {}
+
+
+
+def write_file():
+	write_file = "media_likes.txt"
+	fw = open(write_file, 'w')
+	pickle.dump(media_like, fw)
+	fw.close()
+
+
+
+
+
+f = open(file_name, 'r')
+print("Reading file: " + file_name)
+media = pickle.load(f)
+f.close()
+print("Reading finish! ")
+print(len(media))
+
+
+for key in media:
+	media_id = key[0]
+	likes = len(media[key].likes)
+	media_like[media_id] = likes
+	
+	
+
+	
+write_file()
+
+print('Done.')
+
+
